fcdac5ac49b5370346090565e9c6784d35a8d564
Vinicius Russo <56481183+viniciussrusso@users.noreply.github.com>
2021-09-29 10:04:02 +0100

    commit

--- lesson-02/src/SeasonDisplay.js
+++ lesson-02/src/SeasonDisplay.js
@@ -1,8 +1,19 @@
 import React from "react";
 
+
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9){
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+}; 
+
+
 const SeasonDisplay = (props) => {
-    console.log(props.lat);
-    
+    const season = getSeason(props.season, new Date().getMonth());
+
+    console.log(season);
     return <div> Season Display</div>;
 };
 
