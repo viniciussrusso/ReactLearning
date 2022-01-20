9b86b7f201a74fd1c329a71084629ed9fc83c3c3
Vinicius Russo <56481183+viniciussrusso@users.noreply.github.com>
2022-01-20 08:45:39 +0000

    commit

--- lesson-02/src/Spinner.js
+++ lesson-02/src/Spinner.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-const Spinner = () => {
+const Spinner = (props) => {
 
     return (
         <div class="ui active dimmer">
-            <div class="ui text loader">Loading</div>
+            <div class="ui text loader">{props.message}</div>
         </div>
     );
 };
