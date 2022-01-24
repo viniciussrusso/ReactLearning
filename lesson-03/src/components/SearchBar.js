2692401bec825a5d29d4b8a9ec17efbac7eded4b
Vinicius Russo <56481183+viniciussrusso@users.noreply.github.com>
2022-01-24 11:49:01 +0000

    commit

--- lesson-03/src/components/SearchBar.js
+++ lesson-03/src/components/SearchBar.js
@@ -2,13 +2,26 @@ import React from "react";
 import App from "./App";
 
 class SearchBar extends React.Component {
+    state = { term: ''};
+
+    onFormSubmit(event) {
+        event.preventDefault();
+
+        console.log(this.state.term);
+    }
+    
+
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" />
+                        <input 
+                        type="text" 
+                        value={this.state.term}
+                        onChange={e => 
+                            this.setState({ term: e.target.value.toUpperCase()})} />
                     </div>
                 </form>
             </div>
