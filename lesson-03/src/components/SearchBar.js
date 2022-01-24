a1368f03f1b59eeb84b86e11774886129c712abe
Vinicius Russo <56481183+viniciussrusso@users.noreply.github.com>
2022-01-24 10:55:36 +0000

    commit

---
+++ lesson-03/src/components/SearchBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import App from "./App";
+
+class SearchBar extends React.Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <input type="text" ></input>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
