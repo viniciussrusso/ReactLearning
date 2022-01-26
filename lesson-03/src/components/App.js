39a4e1b1571463869427be659f5beb955f59c126
Vinicius Russo <56481183+viniciussrusso@users.noreply.github.com>
2022-01-26 12:25:42 +0000

    master

--- lesson-03/src/components/App.js
+++ lesson-03/src/components/App.js
@@ -8,8 +8,10 @@ class App extends React.Component {
 
 
     render() {
-        <div className="ui container" style={{marginTop: '10px'}}><SearchBar onSubmit={this.onSearchSubmit}/></div>;
+        return (
+            <div className="ui container" style={{marginTop: '10px'}}><SearchBar onSubmit={this.onSearchSubmit}/></div>
+        );
     }
-};
+}
 
 export default App;
