c0ff64d6928a721b06f930119d06fb95abed01ef
Vinicius Russo <56481183+viniciussrusso@users.noreply.github.com>
2022-02-17 17:28:48 +0000

    search-bar  and    final styling

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: []
-    }
+      monsters: [],
+      searchField: ''
+    };
   }
 
   componentDidMount() {
@@ -17,11 +19,25 @@ class App extends React.Component {
       .then(users => this.setState({ monsters: users }));
   }
 
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
+
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} > 
-        </CardList>
+        <h1> Monsters Rolodex App</h1>
+        <SearchBox 
+          placeholder="search monsters" 
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} > </CardList>
       </div>
     );
   }
